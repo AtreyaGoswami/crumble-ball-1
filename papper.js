a99d9eb8b673a6eee451ba54f7656bc75755f860
AtreyaGoswami <72393490+AtreyaGoswami@users.noreply.github.com>
2020-12-30 20:21:33 +0530

    Update papper.js

--- papper.js
+++ papper.js
@@ -1,4 +1,4 @@
-class papper
+class paper
 {
 	constructor(x,y,r)
 	{
@@ -9,14 +9,29 @@ class papper
 			density:1.2
 
 			
-        }
+			}
 		this.x=x;
 		this.y=y;
 		this.r=r
 		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
 		World.add(world, this.body);
 
-        }
+	}
+	display()
+	{
+			
+			var paperpos=this.body.position;		
+
+			push()
+			translate(paperpos.x, paperpos.y);
+			rectMode(CENTER)
+			ellipse(0,0,this.r, this.r);
+			pop()
+			
+	}
+
 }
+			
+				
 			
 				
